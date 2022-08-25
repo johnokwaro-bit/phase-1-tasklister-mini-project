0a59509a1c77d691abe4cf7468a89367516b2f5b
John Okwaro <john.okwaro@moringaschool.com>
2022-08-25 14:37:02 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // your code here
+  let form = document.querySelector('form')  
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault();    
+    madeToDo(e.target.new_task.value);
+    form.reset()
+    
+
+  })
+  
 });
+
+function madeToDo(toDo){  
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  let li = document.createElement('li');  
+  btn.textContent = 'x';
+  btn.addEventListener('click', handleDelete);  
+  p.textContent = `${toDo} `;
+  p.appendChild(btn);
+  li.appendChild(p)
+  let task = document.querySelector('#tasks');
+  task.appendChild(li);
+}
+
+function handleDelete(e){
+  e.target.parentNode.parentNode.remove();
+}
